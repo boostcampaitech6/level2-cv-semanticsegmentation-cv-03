cb8bc09c835f3709f0ea9e69fdaa6f30d755668f
Jungtaxi <beagentleman7@gmail.com>
2024-02-08 04:43:51 +0900

    feat: Add dice metric
    #18

--- model/metric.py
+++ model/metric.py
@@ -18,3 +18,14 @@ def top_k_acc(output, target, k=3):
         for i in range(k):
             correct += torch.sum(pred[:, i] == target).item()
     return correct / len(target)
+
+
+def dice_coef(outputs, masks):
+    y_true_f = masks.flatten(2)
+    y_pred_f = outputs.flatten(2)
+    intersection = torch.sum(y_true_f * y_pred_f, -1)
+
+    eps = 0.0001
+    return (2.0 * intersection + eps) / (
+        torch.sum(y_true_f, -1) + torch.sum(y_pred_f, -1) + eps
+    )
