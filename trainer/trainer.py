8afdeb1ec676ccefab41c02e789c59ad9f2a39df
rudeuns <rudeuns@gmail.com>
2024-02-07 21:06:19 +0900

    feat: Customize trainer
    #8

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,8 +1,8 @@
-import numpy as np
 import torch
-from torchvision.utils import make_grid
+import torch.nn.functional as F
 from base import BaseTrainer
-from utils import inf_loop, MetricTracker
+from utils import MetricTracker
+from tqdm import tqdm
 
 
 class Trainer(BaseTrainer):
@@ -14,36 +14,33 @@ class Trainer(BaseTrainer):
         self,
         model,
         criterion,
-        metric_ftns,
+        metrics,
         optimizer,
         config,
         device,
-        data_loader,
-        valid_data_loader=None,
+        train_loader,
+        valid_loader=None,
         lr_scheduler=None,
-        len_epoch=None,
     ):
-        super().__init__(model, criterion, metric_ftns, optimizer, config)
-        self.config = config
+        super().__init__(model, optimizer, config)
         self.device = device
-        self.data_loader = data_loader
-        if len_epoch is None:
-            # epoch-based training
-            self.len_epoch = len(self.data_loader)
-        else:
-            # iteration-based training
-            self.data_loader = inf_loop(data_loader)
-            self.len_epoch = len_epoch
-        self.valid_data_loader = valid_data_loader
-        self.do_validation = self.valid_data_loader is not None
-        self.lr_scheduler = lr_scheduler
-        self.log_step = int(np.sqrt(data_loader.batch_size))
 
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+
+        self.train_loader = train_loader
+        self.valid_loader = valid_loader
+        self.do_validation = self.valid_loader is not None
+
+        self.lr_scheduler = lr_scheduler
+
+        self.metrics = metrics
         self.train_metrics = MetricTracker(
-            "loss", *[m.__name__ for m in self.metric_ftns], writer=self.writer
+            "loss", *[m.__name__ for m in self.metrics]
         )
         self.valid_metrics = MetricTracker(
-            "loss", *[m.__name__ for m in self.metric_ftns], writer=self.writer
+            "loss", *[m.__name__ for m in self.metrics]
         )
 
     def _train_epoch(self, epoch):
@@ -53,42 +50,43 @@ class Trainer(BaseTrainer):
         :param epoch: Integer, current training epoch.
         :return: A log that contains average loss and metric in this epoch.
         """
+        self.model.to(self.device)
         self.model.train()
         self.train_metrics.reset()
-        for batch_idx, (data, target) in enumerate(self.data_loader):
-            data, target = data.to(self.device), target.to(self.device)
+
+        for _, (images, masks) in enumerate(
+            tqdm(self.train_loader, desc=f"[Epoch {epoch} (Train)]")
+        ):
+            images, masks = images.to(self.device), masks.to(self.device)
 
             self.optimizer.zero_grad()
-            output = self.model(data)
-            loss = self.criterion(output, target)
+            outputs = self.model(images)["out"]
+            loss = self.criterion(outputs, masks)
             loss.backward()
             self.optimizer.step()
 
-            self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
             self.train_metrics.update("loss", loss.item())
-            for met in self.metric_ftns:
-                self.train_metrics.update(met.__name__, met(output, target))
+            for met in self.metrics:
+                self.train_metrics.update(met.__name__, met(outputs, masks))
 
-            if batch_idx % self.log_step == 0:
-                self.logger.debug(
-                    "Train Epoch: {} {} Loss: {:.6f}".format(
-                        epoch, self._progress(batch_idx), loss.item()
-                    )
-                )
-                self.writer.add_image(
-                    "input", make_grid(data.cpu(), nrow=8, normalize=True)
-                )
-
-            if batch_idx == self.len_epoch:
-                break
         log = self.train_metrics.result()
+
+        for key, value in log.items():
+            print(f"    {key:15s}: {value} |")
 
         if self.do_validation:
             val_log = self._valid_epoch(epoch)
             log.update(**{"val_" + k: v for k, v in val_log.items()})
 
+            for key, value in val_log.items():
+                print(f"    {key:15s}: {value} |")
+
         if self.lr_scheduler is not None:
-            self.lr_scheduler.step()
+            if self.lr_scheduler.__class__.__name__ == "ReduceLROnPlateau":
+                self.lr_scheduler.step(val_log["loss"])
+            else:
+                self.lr_scheduler.step()
+
         return log
 
     def _valid_epoch(self, epoch):
@@ -100,37 +98,29 @@ class Trainer(BaseTrainer):
         """
         self.model.eval()
         self.valid_metrics.reset()
+
         with torch.no_grad():
-            for batch_idx, (data, target) in enumerate(self.valid_data_loader):
-                data, target = data.to(self.device), target.to(self.device)
+            for _, (images, masks) in enumerate(
+                tqdm(self.valid_data_loader, desc=f"[Epoch {epoch} (Valid)]")
+            ):
+                images, masks = images.to(self.device), masks.to(self.device)
 
-                output = self.model(data)
-                loss = self.criterion(output, target)
+                outputs = self.model(images)["out"]
 
-                self.writer.set_step(
-                    (epoch - 1) * len(self.valid_data_loader) + batch_idx,
-                    "valid",
-                )
-                self.valid_metrics.update("loss", loss.item())
-                for met in self.metric_ftns:
-                    self.valid_metrics.update(
-                        met.__name__, met(output, target)
+                output_h, output_w = outputs.size(-2), outputs.size(-1)
+                mask_h, mask_w = masks.size(-2), masks.size(-1)
+
+                if output_h != mask_h or output_w != mask_w:
+                    outputs = F.interpolate(
+                        outputs, size=(mask_h, mask_w), mode="bilinear"
                     )
-                self.writer.add_image(
-                    "input", make_grid(data.cpu(), nrow=8, normalize=True)
-                )
 
-        # add histogram of model parameters to the tensorboard
-        for name, p in self.model.named_parameters():
-            self.writer.add_histogram(name, p, bins="auto")
+                loss = self.criterion(outputs, masks)
+
+                self.valid_metrics.update("loss", loss.item())
+                for met in self.metrics:
+                    self.valid_metrics.update(
+                        met.__name__, met(outputs, masks)
+                    )
+
         return self.valid_metrics.result()
-
-    def _progress(self, batch_idx):
-        base = "[{}/{} ({:.0f}%)]"
-        if hasattr(self.data_loader, "n_samples"):
-            current = batch_idx * self.data_loader.batch_size
-            total = self.data_loader.n_samples
-        else:
-            current = batch_idx
-            total = self.len_epoch
-        return base.format(current, total, 100.0 * current / total)
