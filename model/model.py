8aa3f40a291b24dbe162ee1b67cdb9c94aea02c0
김정택_T6038 <beagentleman7@gmail.com>
2024-02-10 02:22:40 +0900

    feat: Add fcn resnet50 model (#21)
    * feat: Add fcn_resnet50 model
    
    #20
    
    * docs: Write model type in config
    
    #20
    
    * remove: Remove mnist model
    
    #20
    
    * fix: fix the layer
    
    #20
    
    * fix: add last layer
    
    #20

--- model/model.py
+++ model/model.py
@@ -1,22 +1,18 @@
 import torch.nn as nn
-import torch.nn.functional as F
+import torchvision.models.segmentation as models
 from base import BaseModel
 
 
-class MnistModel(BaseModel):
-    def __init__(self, num_classes=10):
+class FCN_ResNet50(BaseModel):
+    def __init__(self, num_classes=29):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_classes)
+        self.pretrained_model = models.fcn_resnet50(
+            weights="DEFAULT",
+        )
+
+        self.pretrained_model.classifier[4] = nn.Conv2d(
+            512, num_classes, kernel_size=1
+        )
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        return self.pretrained_model(x)
