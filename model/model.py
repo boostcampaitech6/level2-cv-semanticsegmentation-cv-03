eaf0e526a93a83ee16deb56d673a30887dd669eb
Jungtaxi <beagentleman7@gmail.com>
2024-02-10 02:19:46 +0900

    fix: add last layer
    #20

--- model/model.py
+++ model/model.py
@@ -1,3 +1,4 @@
+import torch.nn as nn
 import torchvision.models.segmentation as models
 from base import BaseModel
 
@@ -6,7 +7,11 @@ class FCN_ResNet50(BaseModel):
     def __init__(self, num_classes=29):
         super().__init__()
         self.pretrained_model = models.fcn_resnet50(
-            weights="DEFAULT", num_classes=num_classes
+            weights="DEFAULT",
+        )
+
+        self.pretrained_model.classifier[4] = nn.Conv2d(
+            512, num_classes, kernel_size=1
         )
 
     def forward(self, x):
