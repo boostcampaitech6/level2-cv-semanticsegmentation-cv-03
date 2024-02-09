ade3a756451f2ae613fcc11c2239ddf5a29c48d2
Jungtaxi <beagentleman7@gmail.com>
2024-02-09 23:49:23 +0900

    remove: Remove acc metric
    #18

--- model/metric.py
+++ model/metric.py
@@ -1,25 +1,6 @@
 import torch
 
 
-def accuracy(output, target):
-    with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
-        assert pred.shape[0] == len(target)
-        correct = 0
-        correct += torch.sum(pred == target).item()
-    return correct / len(target)
-
-
-def top_k_acc(output, target, k=3):
-    with torch.no_grad():
-        pred = torch.topk(output, k, dim=1)[1]
-        assert pred.shape[0] == len(target)
-        correct = 0
-        for i in range(k):
-            correct += torch.sum(pred[:, i] == target).item()
-    return correct / len(target)
-
-
 def dice_coef(outputs, masks):
     y_true_f = masks.flatten(2)
     y_pred_f = outputs.flatten(2)
