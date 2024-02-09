79b7321b7b28d867a63cb7b075a7ac6e35474dcf
김정택_T6038 <beagentleman7@gmail.com>
2024-02-10 02:13:32 +0900

    feat: Add dice metric (#19)
    * feat: Add dice metric
    
    #18
    
    * docs: Write dice_coef metric in config
    
    #18
    
    * remove: Remove acc metric
    
    #18

--- model/metric.py
+++ model/metric.py
@@ -1,20 +1,12 @@
 import torch
 
 
-def accuracy(output, target):
-    with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
-        assert pred.shape[0] == len(target)
-        correct = 0
-        correct += torch.sum(pred == target).item()
-    return correct / len(target)
+def dice_coef(outputs, masks):
+    y_true_f = masks.flatten(2)
+    y_pred_f = outputs.flatten(2)
+    intersection = torch.sum(y_true_f * y_pred_f, -1)
 
-
-def top_k_acc(output, target, k=3):
-    with torch.no_grad():
-        pred = torch.topk(output, k, dim=1)[1]
-        assert pred.shape[0] == len(target)
-        correct = 0
-        for i in range(k):
-            correct += torch.sum(pred[:, i] == target).item()
-    return correct / len(target)
+    eps = 0.0001
+    return (2.0 * intersection + eps) / (
+        torch.sum(y_true_f, -1) + torch.sum(y_pred_f, -1) + eps
+    )
