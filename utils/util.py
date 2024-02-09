4043e92ffcc3bd0f19d98154f4e1578a6e4fe134
rudeuns <rudeuns@gmail.com>
2024-02-10 03:11:36 +0900

    feat: Add classes list in utils
    #22

--- utils/util.py
+++ utils/util.py
@@ -6,6 +6,39 @@ from itertools import repeat
 from collections import OrderedDict
 
 
+CLASSES = [
+    "finger-1",
+    "finger-2",
+    "finger-3",
+    "finger-4",
+    "finger-5",
+    "finger-6",
+    "finger-7",
+    "finger-8",
+    "finger-9",
+    "finger-10",
+    "finger-11",
+    "finger-12",
+    "finger-13",
+    "finger-14",
+    "finger-15",
+    "finger-16",
+    "finger-17",
+    "finger-18",
+    "finger-19",
+    "Trapezium",
+    "Trapezoid",
+    "Capitate",
+    "Hamate",
+    "Scaphoid",
+    "Lunate",
+    "Triquetrum",
+    "Pisiform",
+    "Radius",
+    "Ulna",
+]
+
+
 def ensure_dir(dirname):
     dirname = Path(dirname)
     if not dirname.is_dir():
