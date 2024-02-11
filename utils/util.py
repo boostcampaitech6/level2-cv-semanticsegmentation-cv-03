6bc98eba804ebf5cbf5d110a429e4eebd5028b20
rudeuns <rudeuns@gmail.com>
2024-02-11 16:59:06 +0900

    remove: Remove not used function
    #24

--- utils/util.py
+++ utils/util.py
@@ -2,7 +2,6 @@ import json
 import torch
 import pandas as pd
 from pathlib import Path
-from itertools import repeat
 from collections import OrderedDict
 
 
@@ -39,12 +38,6 @@ CLASSES = [
 ]
 
 
-def ensure_dir(dirname):
-    dirname = Path(dirname)
-    if not dirname.is_dir():
-        dirname.mkdir(parents=True, exist_ok=False)
-
-
 def read_json(fname):
     fname = Path(fname)
     with fname.open("rt") as handle:
@@ -55,12 +48,6 @@ def write_json(content, fname):
     fname = Path(fname)
     with fname.open("wt") as handle:
         json.dump(content, handle, indent=4, sort_keys=False)
-
-
-def inf_loop(data_loader):
-    """wrapper function for endless data loader."""
-    for loader in repeat(data_loader):
-        yield from loader
 
 
 def prepare_device(n_gpu_use):
