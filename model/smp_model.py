9f2ae33ac76d29badb09eefc443581073a6fad0b
kyungeun <151593264+rudeuns@users.noreply.github.com>
2024-02-25 22:11:05 +0900

    feat: Add smp models (#74)
    * feat: Add Unet++ model
    
    #71
    
    * feat: Add DeepLabV3 model
    
    #71

--- model/smp_model.py
+++ model/smp_model.py
@@ -20,6 +20,24 @@ class Unet(BaseModel):
         return self.pretrained_model(x)
 
 
+class Unet2Plus(BaseModel):
+    def __init__(
+        self,
+        num_classes=29,
+        encoder_name="resnet34",
+        encoder_weights="imagenet",
+    ):
+        super().__init__()
+        self.pretrained_model = smp.UnetPlusPlus(
+            encoder_name=encoder_name,
+            encoder_weights=encoder_weights,
+            classes=num_classes,
+        )
+
+    def forward(self, x):
+        return self.pretrained_model(x)
+
+
 class Linknet(BaseModel):
     def __init__(
         self,
@@ -83,6 +101,24 @@ class PAN(BaseModel):
     ):
         super().__init__()
         self.pretrained_model = smp.PAN(
+            encoder_name=encoder_name,
+            encoder_weights=encoder_weights,
+            classes=num_classes,
+        )
+
+    def forward(self, x):
+        return self.pretrained_model(x)
+
+
+class DeepLabV3(BaseModel):
+    def __init__(
+        self,
+        num_classes=29,
+        encoder_name="resnet34",
+        encoder_weights="imagenet",
+    ):
+        super().__init__()
+        self.pretrained_model = smp.DeepLabV3(
             encoder_name=encoder_name,
             encoder_weights=encoder_weights,
             classes=num_classes,
