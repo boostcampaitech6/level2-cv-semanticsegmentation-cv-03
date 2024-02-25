03b22bdedaf9b00d50547facbc082c46d054c4f5
rudeuns <rudeuns@gmail.com>
2024-02-25 22:08:04 +0900

    feat: Add Unet++ model
    #71

--- model/smp_model.py
+++ model/smp_model.py
@@ -20,6 +20,24 @@ class Unet(BaseModel):
         return self.pretrained_model(x)
 
 
+class Unet2Plus(BaseModel):
+    def __init__(
+        self,
+        num_classes=29,
+        encoder_name="resnet34",
+        encoder_weights="imagenet",
+    ):
+        super().__init__()
+        self.pretrained_model = smp.UnetPlusPlus(
+            encoder_name=encoder_name,
+            encoder_weights=encoder_weights,
+            classes=num_classes,
+        )
+
+    def forward(self, x):
+        return self.pretrained_model(x)
+
+
 class Linknet(BaseModel):
     def __init__(
         self,
